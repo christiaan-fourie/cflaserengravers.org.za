74f93907a2c19a1e3b7eb5088dc1da5393be727f
Christiaan Fourie <christiaanfourie60@gmail.com>
2025-04-07 23:30:12 +0200

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,9 +8,9 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="bg-gray-800 text-white">
-      <div className="container mx-auto flex justify-between items-center p-4">
-        <h1 className="text-xl font-bold">
+    <header className="bg-white/30 backdrop-blur-lg text-white fixed t-0 w-full z-50 shadow-md">
+      <div className="container mx-auto flex justify-between items-center p-4 py-8">
+        <h1 className="text-2xl text-[#d94948]">
           <Link href="/">CF Laser Engravers</Link>
         </h1>
         <button
@@ -23,16 +23,16 @@ export default function Header() {
         <nav
           className={`${
             menuOpen ? 'block' : 'hidden'
-          } absolute top-16 left-0 w-full bg-gray-800 md:static md:block md:w-auto`}
+          } absolute top-16 left-0 w-full md:static md:block md:w-auto`}
         >
           <ul className="flex flex-col md:flex-row md:space-x-6">
             <li className="p-4 md:p-0">
-              <Link href="/" className="hover:text-gray-400">
+              <Link href="/" className="hover:text-gray-400 bg-[#d94948] px-6 py-2 rounded-xl">
                 Home
               </Link>
             </li>
             <li className="p-4 md:p-0">
-              <Link href="/contact" className="hover:text-gray-400">
+              <Link href="/contact" className="hover:text-gray-400 bg-[#d94948] px-6 py-2 rounded-xl">
                 Contact
               </Link>
             </li>
